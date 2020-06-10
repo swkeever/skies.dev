ffbddfa1bb933817917966d1b0a259f068377faa
Sean Keever <swkeever@gmail.com>
2020-06-10 11:12:01 -0700

    adds styles to education, makes theme toggle see through on mobile

--- src/components/cv/Education.tsx
+++ src/components/cv/Education.tsx
@@ -1,21 +1,85 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { FaGraduationCap } from 'react-icons/fa';
+import {
+  FaGraduationCap,
+  FaRegCalendarAlt,
+  FaChartLine,
+  FaBook,
+} from 'react-icons/fa';
 
 function Time({ date }: { date: string }) {
   return (
-    <time
-      dateTime={date}
-      className={`
-        text-center
-        text-onNeutralBgSoft
-      `}
-    >
+    <time dateTime={date} className="text-onNeutralBgSoft inline-block">
       {new Intl.DateTimeFormat('en-US', {
         year: 'numeric',
         month: 'short',
       }).format(new Date(date))}
     </time>
+  );
+}
+
+function TimeBlock({
+  startDate,
+  endDate,
+}: {
+  startDate: string;
+  endDate: string;
+}) {
+  return (
+    <div className="uppercase tracking-widest text-sm mt-2">
+      <FaRegCalendarAlt className="inline text-onNeutralBgSoft mr-1 mb-1" />
+      <Time date={startDate} />
+      <span>&mdash;</span>
+      <Time date={endDate} />
+    </div>
+  );
+}
+
+function Major({ degree, major }: { degree: string; major: string }) {
+  return (
+    <div className="-mt-1">
+      {/* <FaBook className="inline mb-1 text-onNeutralBgSoft mr-1" /> */}
+      <span className=" text-xl tracking-wider">{degree}</span>
+      <span className="uppercase tracking-widest text-sm align-baseline ml-1">
+        {major}
+      </span>
+    </div>
+  );
+}
+
+function GPA({ gpa }: { gpa: string }) {
+  return (
+    <div className="text-onNeutralBgSoft mt-1 md:text-right">
+      <FaChartLine className="inline text-onNeutralBgSoft mb-px mr-1" />
+      <span className="text-sm tracking-wider mr-1">GPA</span>
+      <span>{gpa}</span>
+    </div>
+  );
+}
+
+function School({ institution }: { institution: string }) {
+  return <h3 className="mt-px">{institution}</h3>;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function Coursework({ courses }: { courses: string[] }) {
+  return (
+    <>
+      <h4 className="text-sm font-normal mt-1 text-onNeutralBgSoft">
+        <FaBook className="inline text-onNeutralBgSoft mr-1" />
+        <span className="ml-px">Coursework</span>
+      </h4>
+      <ul className="list-none ml-0 mt-1">
+        {courses.map((c) => (
+          <li
+            key={c}
+            className="inline-block bg-neutralBgSoft rounded-full text-sm px-2 mr-1"
+          >
+            {c}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
@@ -26,6 +90,7 @@ function Item({
   startDate,
   endDate,
   gpa,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   courses,
 }: {
   institution: string;
@@ -34,161 +99,39 @@ function Item({
   startDate: string;
   endDate: string;
   gpa: string;
-  courses: string;
+  courses: string[];
 }) {
   return (
-    <div
-      className={`
-    `}
-    >
-      <h3
-        className={`
-        mt-0
-      `}
-      >
-        {institution}
-      </h3>
-      <div
-        className={`
-        flex
-        space-x-1
-        uppercase
-        tracking-widest
-        text-sm
-      `}
-      >
-        <Time date={startDate} />
-        <span>&mdash;</span>
-        <Time date={endDate} />
-      </div>
-      <div
-        className={`
-        flex
-        justify-between
-        mt-4
-        align-baseline
-      `}
-      >
-        <div
-          className={`
-          
-        `}
-        >
-          <span
-            className={`
-            text-xl
-            tracking-wider
-          `}
-          >
-            {degree}
-          </span>
-          <span
-            className={`
-            uppercase
-            tracking-widest
-            
-            text-sm
-            align-baseline
-            ml-1
-          `}
-          >
-            {major}
-          </span>
+    <div>
+      <div className="flex flex-col md:flex-row justify-between">
+        <div className="flex flex-col justify-between ">
+          <School institution={institution} />
+          <Major degree={degree} major={major} />
         </div>
-        <div
-          className={`
-            font-xl
-            font-light
-        `}
-        >
-          <span
-            className={`
-            mr-px
-            text-sm
-            tracking-wider
-          `}
-          >
-            GPA
-          </span>
-          <span
-            className={`
-            text-xl
-          `}
-          >
-            {gpa}
-          </span>
+        <div className="flex flex-col justify-between">
+          <TimeBlock startDate={startDate} endDate={endDate} />
+          <GPA gpa={gpa} />
         </div>
       </div>
-      <h4
-        className={`
-        text-sm
-        pb-0
-        font-normal
-      `}
-      >
-        Coursework
-      </h4>
-      <ul
-        className={`
-        list-none
-        ml-0
-        mt-1
-      `}
-      >
-        {courses.map((c) => (
-          <li
-            key={c}
-            className={`
-              inline-block
-              bg-neutralBgSoft
-              rounded-full
-              text-sm
-              shadow-inner
-              px-2
-              mr-1
-              
-            `}
-          >
-            {c}
-          </li>
-        ))}
-      </ul>
+      {/* <Coursework courses={courses} /> */}
     </div>
   );
 }
 
 export default function Education({ education }: { education: Object }) {
   return (
-    <>
-      <h2
-        className={`
-
-      `}
-      >
+    <section>
+      <h2>
         Education
         <span>
-          <FaGraduationCap
-            className={`
-            inline
-            ml-2
-            text-onNeutralBgSoft
-            mb-1
-          `}
-          />
+          <FaGraduationCap className="inline ml-2 text-onNeutralBgSoft mb-1" />
         </span>
       </h2>
-      <ul
-        className={`
-          ml-0
-          flex
-          flex-col
-          space-y-4
-        `}
-      >
+      <ul className="ml-0 flex flex-col space-y-4 border-l-2 border-primary pl-4">
         {education.map((i) => (
           <Item key={i.institution} {...i} />
         ))}
       </ul>
-    </>
+    </section>
   );
 }
