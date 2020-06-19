2d525b789540db8ff1a7e3fb9fddd3b0e7c9119c
Sean Keever <swkeever@gmail.com>
2020-06-19 11:44:58 -0700

    update nav test after refactor

--- src/components/__tests__/Nav.test.tsx
+++ src/components/__tests__/Nav.test.tsx
@@ -2,10 +2,10 @@ import { isActive } from '../Nav';
 import routes from '../../utils/routes';
 
 describe('blog path', () => {
-  const blogPath = `${routes.blogs}/personal-statement`;
+  const blogPath = `${routes.learn}/personal-statement`;
 
   test('should be active', () => {
-    expect(isActive(blogPath, routes.blogs)).toBeTruthy();
+    expect(isActive(blogPath, routes.learn)).toBeTruthy();
   });
 
   test('should not be active', () => {
@@ -14,7 +14,7 @@ describe('blog path', () => {
 });
 
 test('ignore trailing slash', () => {
-  expect(isActive(`${routes.resume}/`, routes.resume)).toBeTruthy();
+  expect(isActive(`${routes.about}/`, routes.about)).toBeTruthy();
 });
 
 describe('home page', () => {
@@ -23,7 +23,7 @@ describe('home page', () => {
   });
 
   test('not active', () => {
-    expect(isActive(routes.blogs, routes.home)).toBeFalsy();
+    expect(isActive(routes.learn, routes.home)).toBeFalsy();
     expect(isActive(routes.home, routes.blogs)).toBeFalsy();
   });
 });
---
+++ src/content/binary-search/index.md
@@ -0,0 +1,12 @@
+---
+slug: binary-search
+date: 2020-06-19
+title: Learn about the Binary Search Algorithm with the Dictionary
+description: Binary search can greatly speed up the execution of your programs
+tags:
+  - Algorithms
+  - Computer Science
+image: mobile_app.png
+---
+
+## What is binary search?
