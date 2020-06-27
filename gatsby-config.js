ddbc40fcc73c049751ead0907c2ecd1c00591bc6
Sean Keever <swkeever@gmail.com>
2020-06-26 22:53:12 -0700

    remove gatsby-bg-image from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,13 +55,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-background-image-es5',
-      options: {
-        // add your own characters to escape, replacing the default ':/'
-        specialChars: '/:',
-      },
-    },
-    {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
