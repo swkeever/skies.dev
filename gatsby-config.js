492e7ee13fcee1be31fb64f13c4e1a42be75a70a
Sean Keever <swkeever@gmail.com>
2020-08-01 17:26:26 -0700

    rm emojis

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,18 +35,7 @@ module.exports = {
         extensions: ['css', 'html', 'js', 'svg'],
       },
     },
-    // Add after these plugins if used
-    {
-      resolve: 'gatsby-plugin-purgecss',
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
-        whitelist: [], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      },
-    },
+
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
@@ -103,7 +92,6 @@ module.exports = {
             },
           },
           'gatsby-remark-external-links',
-          'gatsby-remark-a11y-emoji',
           {
             resolve: 'gatsby-remark-autolink-headers',
             options: {
@@ -202,6 +190,18 @@ module.exports = {
             match: '^/blog/',
           },
         ],
+      },
+    },
+    // Add after these plugins if used
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        whitelist: [], // Don't remove this selector
+        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
     'gatsby-plugin-netlify',
