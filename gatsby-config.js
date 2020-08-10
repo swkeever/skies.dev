660a4128f27bb40c438f579a488fbb5154d3abcc
Sean Keever <swkeever@gmail.com>
2020-08-10 09:03:52 -0700

    whitelists classes in purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -207,7 +207,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: [], // Don't remove this selector
+        whitelist: ['gatsby-resp-image-figcaption', 'gatsby-resp-image-figure'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
