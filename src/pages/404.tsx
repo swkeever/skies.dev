88e5004face035324d6524d6c6c3675c6b5154c5
Sean Keever <swkeever@gmail.com>
2020-08-18 21:47:31 -0700

    update 404 page to use default seo image

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,53 +1,31 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import Lost from '../../assets/lost.svg';
 import SEO from '../components/SEO';
 
-const NotFoundPage = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      file(relativePath: { eq: "lost.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 700) {
-            ...GatsbyImageSharpFluid
-            presentationWidth
-            presentationHeight
-          }
-        }
-      }
-    }
-  `);
-
-  return (
-    <>
-      <SEO
-        title="Page not found"
-        image={data.file.childImageSharp.fluid}
-        imageDims={{
-          width: data.file.childImageSharp.fluid.presentationWidth,
-          height: data.file.childImageSharp.fluid.presentationHeight,
-        }}
-        keywords={['Page not found', '404']}
-        description="The page you requested does not exist on our server."
-      />
-      <section className="max-w-screen-xl mx-auto p-4 my-auto">
-        <h1
-          className={`
+const NotFoundPage = () => (
+  <>
+    <SEO
+      title="Page not found"
+      keywords={['Page not found', '404']}
+      description="This is the 404 page. Not much to see here."
+    />
+    <section className="max-w-screen-xl mx-auto p-4 my-auto">
+      <h1
+        className={`
             text-lg
             md:text-xl
             text-neutralSoft
             text-center
             mb-4 xl:mb-8
           `}
-        >
-          404 &ndash; Page not found.
-        </h1>
-        <div className="w-11/12 md:w-9/12 max-w-6xl mx-auto">
-          <Lost className="w-full h-full" />
-        </div>
-      </section>
-    </>
-  );
-};
+      >
+        404 &ndash; Page not found.
+      </h1>
+      <div className="w-11/12 md:w-9/12 max-w-6xl mx-auto">
+        <Lost className="w-full h-full" />
+      </div>
+    </section>
+  </>
+);
 
 export default NotFoundPage;
