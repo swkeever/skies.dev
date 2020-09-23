b3b52a11b9e7d8b5a2d18589a3f66914be938aeb
Sean Keever <swkeever@gmail.com>
2020-09-22 18:45:02 -0700

    update transitions

--- content/tailwind-flexbox/utils.ts
+++ content/tailwind-flexbox/utils.ts
@@ -1,7 +1,12 @@
+import globalStyles from '../../src/styles';
+import tw from '../../src/utils/tailwind';
+
 export const styles = {
   container: 'my-8 lg:my-12 p-2 bg-neutralBgSoft rounded-md shadow-inner',
-  li:
+  li: tw(
     'rounded-md border border-neutralBgSofter p-3 text-onPrimaryBg bg-primaryBgSoft',
+    globalStyles.transitions,
+  ),
 };
 export const threeItems = ['Uno', 'Dos', 'Tres'];
 export const fiveItems = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
