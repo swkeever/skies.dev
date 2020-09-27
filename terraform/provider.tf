eadbff1d44dc4207260aa2ea4bb1b878013fb945
Sean Keever <swkeever@gmail.com>
2020-09-27 15:59:49 -0700

    update redirect lambda, update siteurl

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,6 +9,5 @@ locals {
   region = "us-east-1"
   referer_key = "wQiCxJVmq0XZV99ZIvysnlwvv89QUxnOjRIa"
   cloudfront_ttl = 31536000
-  redirect_lambda_arn = "arn:aws:lambda:us-east-1:674770168879:function:WWWRedirect:1"
 }
 
