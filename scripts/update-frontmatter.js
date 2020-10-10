4a0265e8cd967b4757d6cf8026f247bcccfa7163
Sean Keever <swkeever@gmail.com>
2020-10-10 10:06:08 -0700

    fix lint issue

--- scripts/update-frontmatter.js
+++ scripts/update-frontmatter.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const path = require('path');
 const matter = require('gray-matter');
 const util = require('util');
+const YAML = require('yaml');
+const blogCategories = require('../src/utils/blog-categories');
+const authors = require('../src/utils/authors');
 
 const READ_PATH = path.join(process.cwd(), 'content');
 const WRITE_PATH = path.join(process.cwd(), 'tmp_content');
@@ -26,23 +29,70 @@ async function getContent() {
   return files.filter((f) => f.endsWith('/index.mdx'));
 }
 
+function getDateFormat(dateObject) {
+  // adjust 0 before single digit date
+  const date = (`0${dateObject.getDate()}`).slice(-2);
+
+  // current month
+  const month = (`0${dateObject.getMonth() + 1}`).slice(-2);
+
+  // current year
+  const year = dateObject.getFullYear();
+
+  return `${year}-${month}-${date}`;
+}
+
 // this function will change depending on on the
 // current shape of the frontmatter, and the desired shape
-function transformFrontmatter(front) {}
+function transformFrontmatter(front) {
+  return {
+    date: {
+      published: getDateFormat(front.datePublished),
+      modified: getDateFormat(front.dateModified),
+    },
+    title: front.title,
+    description: front.description,
+    categoryId: front.category,
+    authorId: 0,
+    keywords: front.keywords,
+    tags: front.tags,
+    image: {
+      src: front.imageUrl,
+      photographer: front.imagePhotographer,
+    },
+  };
+}
 
+const TESTING = false;
 async function main() {
   const blogFiles = await getContent();
   blogFiles.forEach((file) => {
     const source = fs.readFileSync(file);
     const { content, data } = matter(source);
     const frontTransformed = transformFrontmatter(data);
-    console.log(frontTransformed);
-    if (util.isDeepStrictEqual(data, frontTransformed)) {
-      console.info('frontmatter already transformed');
-      return;
+
+    if (TESTING) {
+      const dir = `./tmp_content/${file.substring(
+        file.indexOf('content/') + 'content/'.length,
+        file.indexOf('/index.mdx'),
+      )}/`;
+      console.log(dir);
+
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+      }
     }
-    const writeToPath = file.replace(READ_PATH, WRITE_PATH);
-    console.log('writing to', writeToPath);
+
+    const writeToPath = TESTING
+      ? file.replace(READ_PATH, WRITE_PATH)
+      : READ_PATH;
+
+    let writeData = '---\n';
+    writeData += YAML.stringify(frontTransformed);
+    writeData += '---\n';
+    writeData += content;
+
+    fs.writeFileSync(writeToPath, writeData);
   });
 }
 
